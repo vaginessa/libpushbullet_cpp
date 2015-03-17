ba6011d152520c45179afb8fdd6449756db321bf
Henri Buyse <henri.buyse@gmail.com>
2015-03-17 19:52:23 +0100

    Can now push a note from the cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,114 @@
 #include "PushBullet.hpp"
 
-#define VERSION 0.0.1
+#define VERSION "0.0.1"
 
-using namespace std;
-namespace po = boost::program_options;
+
+// namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
 
 #ifdef _DEBUG_
     std::cout << "Mode:   DEBUG" << std::endl;
 #endif
-/*
+
+    boost::program_options::variables_map vm;
+    boost::program_options::options_description visible("Allowed options");
+    boost::program_options::options_description generic("Generic options");
+    boost::program_options::options_description type("Type of push");
+    boost::program_options::options_description param("Parameters");
+
+    std::string device;
+    std::string title;
+    std::string body;
+    std::string url;
+    std::string path_file;
+
+
     try {
-        po::options_description generic("generic options");
-        desc.add_options()
-            ("help,h", "produce this help message")
-            ("version,v", "print program's version")
+        /* Add the different options to generic options
+         */
+        generic.add_options()
+            ("help,h",    "Produce this help message.")
+            ("version,v", "Print program's version.")
         ;
 
-        po::variables_map vm;        
-        po::store(po::parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);    
+        type.add_options()
+            ("note,n", "Push a note.")
+            ("link,l", "Push a link.")
+            ("file,f", "Push a file.")
+        ;
 
-        if (vm.count("help")) {
-            cout << desc << "\n";
-            return 0;
-        }
+        param.add_options()
+            ("device,d",
+                boost::program_options::value<std::string>(&device)->default_value("All devices"),
+                "Send to a specific device (designated by its name).")
+            ("title,t",
+                boost::program_options::value<std::string>(&title),
+                "Title of the push.")
+            ("body,b",
+                boost::program_options::value<std::string>(&body),
+                "Body of the push.")
+            ("url,u",
+                boost::program_options::value<std::string>(&url),
+                "URL that will be send push.")
+            ("path-file,p",
+                boost::program_options::value<std::string>(&path_file),
+                "Path to the file you want to push.")
+        ;
 
-        if (vm.count("compression")) {
-            cout << "Compression level was set to " 
-                 << vm["compression"].as<double>() << ".\n";
-        } else {
-            cout << "Compression level was not set.\n";
-        }
+        visible.add(generic).add(type).add(param);
+
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, visible), vm);
+        boost::program_options::notify(vm);
+
+
     }
-    catch(exception& e) {
-        cerr << "error: " << e.what() << "\n";
-        return 1;
+    catch(const std::exception& e) {
+        std::cerr << "ERROR > " << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
     catch(...) {
-        cerr << "Exception of unknown type!\n";
-    }
-*/
-    try {
-        PushBullet  pb;
-    
-        pb.get_user_informations();
-    
-        #ifdef _DEBUG_
-        pb.display_user_informations();
-        pb.display_devices();
-        #endif
-    }
-    catch (const std::exception& e) {
-        std::cerr << "ERROR > " << e.what() << std::endl;
-        return -1; 
+        std::cerr << "Exception of unknown type!" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    // pb.get_all_devices();
 
-    // std::string example = "{\"type\": \"note\", \"title\": \"Note Title\", \"body\": \"Note Body\"}";
-    // pb.pushing(example);
+    if (vm.count("help")) {
+        std::cout << visible << std::endl;
+        return EXIT_SUCCESS;
+    }
 
-    // std::string title = "Toto";
-    // std::string body = "Toto est dans la place n3";
-    // pb.note(title, body, "ujEIL5AaxhYsjAiVsKnSTs");
-
-
-    // std::string body = "URL Body";
-    // std::string title = "URL Title";
-    // std::string url = "https://www.google.fr";
-    // pb.link(title, body, url);
+    if (vm.count("version")) {
+        std::cout << "Pushbullet v" << VERSION << std::endl;
+        return EXIT_SUCCESS;
+    }
 
 
-    // pb.display_devices();
-    // pb.create_device_if_not_existing();
+    if (vm.count("note") && vm.count("body") && vm.count("title")) {
+        std::cout << "Title  : " << title << std::endl;
+        std::cout << "Body   : " << body  << std::endl;
+        std::cout << "Device : " << device  << std::endl;
 
-    // pb.display_devices();
+        try {
+            PushBullet  pb;
 
-    // pb.delete_device("AO725");
+            /* Download basic informations
+             */
+            pb.download_user_informations();
+            pb.download_all_devices();
 
-    // pb.get_all_devices();
-    // pb.display_devices();
+            /* Send a note
+             */
+            pb.note(title, body);
 
+        }
+        catch (const std::exception& e) {
+            std::cerr << "PUSHBULLET_CREATION > " << e.what() << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        return EXIT_SUCCESS;
+    }
 
     return EXIT_SUCCESS;
 }
